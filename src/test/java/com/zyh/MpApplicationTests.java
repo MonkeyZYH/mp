10a4e0b7452664cf561c1c6a7e6e4c3e1a29a231
zyh <931270186@qq.com>
2022-07-21 15:20:14 +0800

    2

--- src/test/java/com/zyh/MpApplicationTests.java
+++ src/test/java/com/zyh/MpApplicationTests.java
@@ -6,12 +6,37 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
 import java.util.List;
 
 @SpringBootTest
 class MpApplicationTests {
     @Autowired
     private EmpDao empDao;
+
+    @Test
+    void testSave(){
+        emp emp = new emp();
+        emp.setId(18);
+        emp.setName("东方不败");
+        emp.setAge(24);
+        emp.setJob("职员");
+        emp.setSalary(12000);
+        emp.setEntrydate(new Date(System.currentTimeMillis()));
+        emp.setManagerid(10);
+        emp.setDeptId(4);
+        empDao.insert(emp);
+    }
+    @Test
+    void testDelete(){
+        empDao.deleteById(18);
+    }
+
+    @Test
+    void testGetById() {
+        emp emp = empDao.selectById(3);
+        System.out.println(emp);
+    }
 
     @Test
     void testgetAll() {
